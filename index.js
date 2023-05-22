2b48b75277ba24e1cdd1d9404a891266109cd25c
alamin360am <alaminhimu0@gmail.com>
2023-05-22 20:59:02 +0600

    delet prblm solve

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-const express = require("express");
-const cors = require("cors");
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
-require("dotenv").config();
 
-// Middleware
-
+// middleware
 app.use(cors());
 app.use(express.json());
 
@@ -42,11 +41,18 @@ async function run() {
     app.get("/added_toy", async (req, res) => {
       console.log(req.query.seller_email);
       let query = {};
-      if(req.query?.seller_email) {
-        query = {seller_email: req.query.seller_email}
+      if (req.query?.seller_email) {
+        query = { seller_email: req.query.seller_email };
       }
       const cursor = AddedToyCollection.find(query);
       const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/added_toy/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await AddedToyCollection.findOne(query);
       res.send(result);
     });
 
@@ -73,11 +79,11 @@ async function run() {
 
     // Delete
 
-    app.delete("/toys/:id", async (req, res) => {
+    app.delete("added_toy/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
-      const result = await toyCollection.deleteMany(query);
-      res.send(result);
+      const result = await AddedToyCollection.deleteOne(query);
+      res.send(result)
     });
 
     // Send a ping to confirm a successful connection
